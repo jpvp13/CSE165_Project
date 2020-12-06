a0b86d786fdaa15b097ef7210265faaea07c44a9
John Villalvazo <jvillalvazo2@ucmerced.edu>
2020-12-06 14:37:56 -0800

    Created menu and transitions into Game

---
+++ StartScreen.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+#include "StartScreen.h"
+
+static StartScreen* singleton;
+
+void StartScreentimer(int id){
+    singleton->turtleL->advance();
+    singleton->turtleR->advance();
+    singleton->redraw();
+
+    if(singleton->turtleL->isDone()){
+        singleton->turtleL->reset();
+    }
+    if(singleton->turtleR->isDone()){
+        singleton->turtleR->reset();
+    }
+
+    glutTimerFunc(singleton->turtleInterval, StartScreentimer, id);
+}
+
+
+StartScreen::StartScreen(){
+    blackScreen = new Rect(-1.0, 1.0, 2.0, 2.0, 0.0, 0.0, 0.0);
+    menu = new TexRect("images/TurtlerMenu.png", -1.0 , 1.0, 1.9, 1.9);
+    turtleL = new Sprite("images/turtleSprite.png", 1,4, -0.9, 0.5, 0.2, 0.2);
+    turtleR = new Sprite("images/turtleSprite.png", 1,4, 0.6, 0.5, 0.2, 0.2);
+
+    singleton = this;
+    turtleInterval = 250;
+    StartScreentimer(1);
+}
+
+void StartScreen::screenDraw(){
+    
+    blackScreen->draw();
+    turtleL->draw();
+    turtleR->draw();
+    menu->draw();
+
+}
+
+StartScreen::~StartScreen(){
+    delete menu;
+    delete blackScreen;
+    delete turtleL;
+    delete turtleR;
+    std::cout << "Deleting Menu ..." << std::endl;
+}
+
---
+++ StartScreen.h
@@ -0,0 +1,35 @@
+#ifndef STARTSCREEN_H
+#define STARTSCREEN_H
+
+#include "Rect.h"
+#include <deque>
+#include <vector>
+#include "TexRect.h"
+#include "Shape.h"
+#include "Sprite.h"
+
+
+class StartScreen : public Rect{
+
+    Sprite* turtleL;
+    Sprite* turtleR;
+    Rect* blackScreen;
+    TexRect* menu;
+
+public:
+    StartScreen();
+
+    void screenDraw();
+
+    friend void StartScreentimer(int id);
+
+    unsigned int turtleInterval;
+
+    bool visable;
+
+    ~StartScreen();
+
+
+};
+
+#endif
